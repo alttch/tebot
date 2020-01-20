feace0a22fd5730ff8cb48b1c7a74c5c2da0b7ef
Sergei S <div@altertech.com>
2020-01-20 21:40:49 +0000

    0.1.5

--- tebot/__init__.py
+++ tebot/__init__.py
@@ -1,6 +1,6 @@
 __author__ = 'Altertech, https://www.altertech.com/'
 __copyright__ = 'Copyright (c) 2020 Altertech'
 __license__ = 'MIT'
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 
 from .bot import TeBot
